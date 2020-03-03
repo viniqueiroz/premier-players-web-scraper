c729f3326637d17f8e1708a2128fb61aa7133c00
Vinicius <vinicius.moura@convergenceworks.com.br>
2020-03-03 12:05:01 -0300

    Ajusta Ordem do processamento da tabela e finaliza criação do objeto de resultado

---
+++ README.md
@@ -0,0 +1,14 @@
+# Scraper dos top Goals da Premier League
+
+## Instalação
+
+ 1. Rodar comando `npm intall`
+ 
+ ## Execução
+  1. Rodar comando `npm start`
+
+
+## Orientações
+
+ - Os arquivos de resultado ficam na pasta /dump
+ - O resultado geral é o arquivo **result.json**/**result.txt**
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
+var json2xls = require('json2xls');
 const fs = require("fs");
+// URL alvo do scraper
 const url =
     "https://www.premierleague.com/stats/top/players/goals?se=-1&cl=-1&iso=-1&po=-1?se=-1";
 
@@ -19,60 +21,83 @@ const options = {
     ignoreHTTPSErrors: true,
     userDataDir: "./tmp"
 };
-const topPremierLeagueScorers = [];
+let topPremierLeagueScorers = [];
+let nacionalidades = {};
+let result = {};
 const nextSelector = "div.paginationBtn.paginationNextContainer";
 const previousSelector = "div.paginationBtn.paginationPreviousContainer";
 let goNext = true;
 let html;
-let isEmpty;
+let isEmpty = false;
+let pagina = 0;
 (async () => {
+    // Inicia o Puppeteer
     const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
+
     await page.setViewport({
         width: 768,
         height: 1024
     });
+
     html = await page.goto(url).then(function () {
         return page.content();
     });
-    await page.waitFor(500);
-    isEmpty = processTable(html);
+
     await page.waitForSelector(nextSelector, {
         timeout: 10000
     });
 
+    // Itera a tabela de players
     while (goNext) {
         await page.waitFor(500);
+        html = await page.content();
+        // Checa de a página atual dbela está vazia
         if (isEmpty) {
+            // Clica no botão voltar para recarregar a página
             await page.waitForSelector(previousSelector, {
                 timeout: 10000
             });
             await click(previousSelector, page);
+            pagina--;
             await page.waitFor(500);
         } else {
+            // Avança uma página
             await page.waitForSelector(nextSelector, {
                 timeout: 10000
             });
             await click(nextSelector, page);
+            pagina++;
             await page.waitFor(500);
         }
+        // Processa a página atual da tabela de players
         isEmpty = processTable(html);
-        html = await page.content();
     }
+    // Encerra o puppeteer
     await browser.close();
-    fs.writeFileSync(
-        "dump/greatestTopPlayers.json",
-        JSON.stringify(topPremierLeagueScorers)
-    );
+    // Ajusta objeto de resultado
+    result = { topPremierLeagueScorers,Nacionalidades: nacionalidades };
+
+    writeFile("dump/greatestTopPlayers.json", topPremierLeagueScorers);
+    writeFile("dump/nacionalidades.json", nacionalidades);
+    writeFile("dump/result.json", result);
+    writeFile("dump/result.txt", result);
+
+    console.log("Arquivo de resultados Gravado na pasta /dump !");
+
+
 })();
 
 function processTable(html) {
+    console.log("Processando Página: " + pagina);
+
     const $ = cheerio.load(html);
     const statsTable = $(".statsTableContainer > tr");
     if (statsTable.length == 1) {
+        console.log("Página: " + pagina + " Vazia!");
         return true;
     }
-
+    // Itera os itens da tabela
     statsTable.each(function () {
         const rank = $(this)
             .find(".rank > strong")
@@ -92,14 +117,17 @@ function processTable(html) {
             nationality,
             goals
         });
+        // Incrementa quantidade de players daquela nacionalidade
+        nacionalidades[nationality] = (nacionalidades[nationality]) ? nacionalidades[nationality] + 1 : 1;
     });
 
     if ($(nextSelector + '.inactive').length > 0) {
+        console.log("Processamento Concluído!");
         goNext = false;
     }
     return false;
 }
-
+// Executa clique na tela
 async function click(selector, page) {
     try {
         await page
@@ -113,3 +141,11 @@ async function click(selector, page) {
         throw err;
     }
 }
+// Escreve JSON em arquivo
+function writeFile(fileName, json) {
+    fs.writeFileSync(
+        fileName,
+        JSON.stringify(json)
+    );
+}
+
